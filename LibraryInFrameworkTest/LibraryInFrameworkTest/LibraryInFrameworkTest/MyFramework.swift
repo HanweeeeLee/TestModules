dc4bbd27caea690d472d806662ef0b5598ea28fe
LEE HANWE <60125719+HanweeeeLee@users.noreply.github.com>
2020-07-02 11:14:16 +0900

    Add files via upload
    objc로 작성된 정적라이브러리를 swift framework에 넣고싶음

---
+++ LibraryInFrameworkTest/LibraryInFrameworkTest/LibraryInFrameworkTest/MyFramework.swift
@@ -0,0 +1,26 @@
+//
+//  MyFramework.swift
+//  LibraryInFrameworkTest
+//
+//  Created by hanwe lee on 2020/07/02.
+//  Copyright © 2020 hanwe. All rights reserved.
+//
+
+import LibraryInFrameworkTest
+
+public class MyFramework {
+    public func test() {
+        print("hello world")
+    }
+    public init() {
+        
+    }
+    public func genSafePrimeNumber(bits:Int) -> Int { //test
+        var n = BN_new()
+        if BN_generate_prime_ex(n, Int32(bits), 1, nil, nil, nil) != 1 {
+            return 0
+        }
+        print("test:\(String(describing: n))")
+        return 1
+    }
+}
