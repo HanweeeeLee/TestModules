c70becbadc4e2b430af4c3c0ec9d6a865fbb04a8
LEE HANWE <60125719+HanweeeeLee@users.noreply.github.com>
2020-07-13 10:15:32 +0900

    Add files via upload

---
+++ Base58OpensourceTest/Pods/Target Support Files/Pods-Base58OpensourceTest-Base58OpensourceTestUITests/Pods-Base58OpensourceTest-Base58OpensourceTestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Base58OpensourceTest_Base58OpensourceTestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Base58OpensourceTest_Base58OpensourceTestUITests
+@end
---
+++ Base58OpensourceTest/Pods/Target Support Files/Pods-Base58OpensourceTestTests/Pods-Base58OpensourceTestTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Base58OpensourceTestTests : NSObject
+@end
+@implementation PodsDummy_Pods_Base58OpensourceTestTests
+@end
