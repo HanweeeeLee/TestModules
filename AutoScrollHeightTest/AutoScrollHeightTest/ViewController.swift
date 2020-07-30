726375e10c5c90a505a4e36259e435360574dc56
LEE HANWE <60125719+HanweeeeLee@users.noreply.github.com>
2020-07-30 14:06:15 +0900

    Add files via upload

---
+++ AutoScrollHeightTest/AutoScrollHeightTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AutoScrollHeightTest
+//
+//  Created by hanwe lee on 2020/07/30.
+//  Copyright © 2020 hanwe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myLabel.text = "a\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nb"
+        myLabel.numberOfLines = .max
+    }
+
+
+}
+
