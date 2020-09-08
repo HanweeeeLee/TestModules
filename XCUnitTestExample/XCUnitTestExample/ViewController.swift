43041f43444f107fb6d228a6fdcdf4b120b8a0fa
LEE HANWE <60125719+HanweeeeLee@users.noreply.github.com>
2020-09-08 11:17:56 +0900

    Add files via upload
    출처 : https://zeddios.tistory.com/48

---
+++ XCUnitTestExample/XCUnitTestExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCUnitTestExample
+//
+//  Created by hanwe lee on 2020/09/08.
+//  Copyright © 2020 hanwe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
