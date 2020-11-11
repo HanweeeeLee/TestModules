4ae3aa9989a822439cb9c699e4a6e1b2e971af2f
LEE HANWE <mobiledev@kakao.com>
2020-11-11 13:45:17 +0900

    Update ViewController.swift
    pair

--- WatchAppFirst/WatchAppFirst/ViewController.swift
+++ WatchAppFirst/WatchAppFirst/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import WatchConnectivity
 
 class ViewController: UIViewController {
-
+    
+    var mySession: WCSession = WCSession.default
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if WCSession.isSupported() {
+            mySession.delegate = self
+            mySession.activate()
+            
+        }
     }
 
 
 }
 
+extension ViewController: WCSessionDelegate{
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        print("test1")
+        if session.activationState == .activated {
+            if session.isWatchAppInstalled {
+                print("호우! : \(self.mySession) ddddddd   \(session)")
+                if session.isPaired {
+                    print("isPaired")
+                    print("activationState :\(session.activationState)")
+                }
+                else {
+                    print("is not isPaired")
+                }
+            }
+        }
+        else if session.activationState == .inactive {
+            
+        }
+        else {
+            
+        }
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        print("test2")
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        print("test3")
+    }
+}
