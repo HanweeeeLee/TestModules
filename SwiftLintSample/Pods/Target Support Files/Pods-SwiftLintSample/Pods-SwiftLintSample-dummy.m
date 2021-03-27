8fd2be53bb44743e5ce57ac985095dd2a2a09cbc
Hanwe Lee <mobiledev@kakao.com>
2021-03-27 22:37:46 +0900

    SwiftLint with CocoaPod

---
+++ SwiftLintSample/Pods/Target Support Files/Pods-SwiftLintSample/Pods-SwiftLintSample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftLintSample : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftLintSample
+@end
---
+++ SwiftLintSample/SwiftLintSample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SwiftLintSample
+//
+//  Created by hanwe on 2021/03/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var myStr:String = "" //콜론 이상하게 찍으니 경고가 뜬다.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
