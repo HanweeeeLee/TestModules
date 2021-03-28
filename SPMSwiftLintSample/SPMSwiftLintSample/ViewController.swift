acf13ec579f57b0eafe685c7f1d7a513a9969de2
Hanwe Lee <mobiledev@kakao.com>
2021-03-28 17:44:52 +0900

    SPM SwiftLint

---
+++ SPMSwiftLintSample/SPMSwiftLintSample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SPMSwiftLintSample
+//
+//  Created by hanwe on 2021/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var myStr:String = "" // waring
+    var myStr2: String = "" // not waring
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
