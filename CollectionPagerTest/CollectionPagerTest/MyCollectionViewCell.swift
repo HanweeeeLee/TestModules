1008d11fa358c8050a9211c0bfffeb5506ee93dd
Hanwe Lee <mobiledev@kakao.com>
2021-04-18 00:39:58 +0900

    no message

---
+++ CollectionPagerTest/CollectionPagerTest/MyCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionPagerTest
+//
+//  Created by hanwe on 2021/04/17.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ CollectionPagerTest/CollectionPagerTest/ViewController.swift
@@ -0,0 +1,120 @@
+//
+//  ViewController.swift
+//  CollectionPagerTest
+//
+//  Created by hanwe on 2021/04/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+//    var leftRightEdgeInset: CGFloat = 20
+    var interitemSpacing: CGFloat = 10
+    let minimumLineSpacing: CGFloat = 20
+    var currentIndex: CGFloat = 0
+    
+    let cellRatio: CGFloat = 0.7
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        self.collectionView.register(UINib(nibName: "MyCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "MyCollectionViewCell")
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = minimumLineSpacing
+        layout.minimumInteritemSpacing = interitemSpacing
+        // width, height 설정
+        let cellWidth = floor(UIScreen.main.bounds.width * cellRatio)
+        let cellHeight = floor(UIScreen.main.bounds.height * cellRatio)
+        layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
+        print("test:\(UIScreen.main.bounds.width * cellRatio)")
+        
+        
+        // 상하, 좌우 inset value 설정
+        let insetX = (view.bounds.width - cellWidth) / 2.0
+        let insetY = (view.bounds.height - cellHeight) / 2.0
+        collectionView.contentInset = UIEdgeInsets(top: insetY, left: insetX, bottom: insetY, right: insetX)
+        self.collectionView.collectionViewLayout = layout
+        self.collectionView.showsHorizontalScrollIndicator = false
+        self.collectionView.showsVerticalScrollIndicator = false
+        
+        collectionView.decelerationRate = UIScrollView.DecelerationRate.fast
+    }
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let resultCnt = 100
+        
+        return resultCnt
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCollectionViewCell", for: indexPath) as! MyCollectionViewCell
+        if indexPath.item%2 == 0 {
+            cell.contentView.backgroundColor = .gray
+        }
+        else {
+            cell.contentView.backgroundColor = .lightGray
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let cellSize: CGSize = CGSize(width: UIScreen.main.bounds.width * cellRatio, height: UIScreen.main.bounds.width * cellRatio)
+//        print("cellSize:\(cellSize.width)")
+//        return cellSize
+//    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>)
+    {
+        // item의 사이즈와 item 간의 간격 사이즈를 구해서 하나의 item 크기로 설정.
+        let layout = self.collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        let cellWidthIncludingSpacing = floor(UIScreen.main.bounds.width * cellRatio) + layout.minimumLineSpacing
+        
+        // targetContentOff을 이용하여 x좌표가 얼마나 이동했는지 확인
+        // 이동한 x좌표 값과 item의 크기를 비교하여 몇 페이징이 될 것인지 값 설정
+        var offset = targetContentOffset.pointee
+        let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
+        var roundedIndex = round(index)
+        
+        // scrollView, targetContentOffset의 좌표 값으로 스크롤 방향을 알 수 있다.
+        // index를 반올림하여 사용하면 item의 절반 사이즈만큼 스크롤을 해야 페이징이 된다.
+        // 스크로로 방향을 체크하여 올림,내림을 사용하면 좀 더 자연스러운 페이징 효과를 낼 수 있다.
+        if scrollView.contentOffset.x > targetContentOffset.pointee.x {
+            roundedIndex = floor(index)
+        } else if scrollView.contentOffset.x < targetContentOffset.pointee.x {
+            roundedIndex = ceil(index)
+        } else {
+            roundedIndex = round(index)
+        }
+        
+//        if isOneStepPaging {
+            if currentIndex > roundedIndex {
+                currentIndex -= 1
+                roundedIndex = currentIndex
+            } else if currentIndex < roundedIndex {
+                currentIndex += 1
+                roundedIndex = currentIndex
+            }
+//        }
+        
+        print("roundedIndex:\(roundedIndex)")
+        
+        // 위 코드를 통해 페이징 될 좌표값을 targetContentOffset에 대입하면 된다.
+        offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - (scrollView.contentInset.left), y: -scrollView.contentInset.top)
+        print("offset:\(offset)")
+        targetContentOffset.pointee = offset
+    }
+    
+}
+
