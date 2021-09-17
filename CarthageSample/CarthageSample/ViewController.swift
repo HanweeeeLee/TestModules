07e2e021bb67fa9d3eae62bf16a67aada9455066
LEE HANWE <mobiledev@kakao.com>
2021-09-17 11:45:34 +0900

    carthage

---
+++ CarthageSample/CarthageSample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CarthageSample
+//
+//  Created by hanwe on 2021/09/17.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+
+    var observableInt: Observable<Int>?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
